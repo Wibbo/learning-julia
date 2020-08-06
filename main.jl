f826849ea773215cdeb975ecdaea851230c2be7f
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2020-08-06 14:40:49 +0100

    Added create person function.

--- main.jl
+++ main.jl
@@ -1,20 +1,17 @@
 include("corona.jl")
 
-# A place to store people.
-people = Array{Person, 1}
+people = Array{corona.Person}(undef, 110)
 
-for i in 1:10
+function create_person(name::String, status::corona.Status)
+    pos = corona.Position(rand(1:100), rand(1:100))
+    radius = rand(5:20)
 
-    pos = corona.Position(rand(1:1000), rand(1:1000))
-    radius = corona.Infection_Radius(rand(5:20), rand(5:20))
-    p1 = corona.Person("Person $i",
-                       pos,
-                       radius,
-                       corona.Healthy)
+    return corona.Person(name, pos, radius, status)
+end
 
-    println("Name $(p1.name) at ($(p1.position.x),$(p1.position.y))
-        with radius ($(p1.radius.width),$(p1.radius.height))
-        is $(p1.status)")
-
-
+for i in 1:100
+    p = create_person("Person $i", corona.Healthy)
+    pos1 = p.position.x
+    pos2 = p.position.y
+    println("$(p.name) at ($(pos1),$(pos2)) with radius ($(p.radius)) is $(p.status)")
 end
