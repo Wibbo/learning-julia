91781bdd2edc2242a4086cf2cef0c843d81f9232
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2020-08-06 14:01:17 +0100

    Added collision capability.

--- corona.jl
+++ corona.jl
@@ -1,25 +1,65 @@
+# Create a module to avoid namespace collisions.
 module corona
 
-    mutable struct Position
-        x::Int
-        y::Int
-    end
-
-    struct InfectionRadius
-        width::Int
-        height::Int
-    end
-
-    struct Size
-        width::Int
-        height::Int
-    end
-
+# All living entities are a Creature.
 abstract type Creature end
 
-position(c::Creature) = c.position
-size(c::Creature) = c.size
-infection_radius(c::Creature) = c.infection_radius
-shape(c::Creature) = :unknown
+# Define the possible statuses for a person.
+@enum Status Healthy Infected Immune
 
-end # module
+# Create a struct to hold a person's position.
+mutable struct Position
+    x::Int
+    y::Int
+end
+
+# Create a struct to define each person's radius of infection.
+struct Infection_Radius
+    width::Int
+    height::Int
+end
+
+# Define the physical bounds of each person.
+struct Creature_Area
+    top::Int
+    left::Int
+    bottom::Int
+    right::Int
+
+    Creature_Area(p::Position, r::Infection_Radius) =
+        new(p.y+r.height, p.x, p.y, p.x+r.width)
+end
+
+"""
+Determines if a person is within someone's infection radius.
+Returns True if they are and False otherwise.
+"""
+function distance_violated(P1::Creature_Area, P2::Creature_Area)
+    return P1.left < P2.right && P1.right > P2.left &&
+        P1.top > P2.bottom && P1.bottom < P2.top
+end
+
+function infection_risk(creature1::Creature, creature2::Creature)
+    println("Checking collision of thing vs. thing")
+    creature1_space = Creature_Area(position(creature1), size(creature1))
+    creature2_space = Creature_Area(position(creature2), size(creature2))
+    return distance_violated(rectA, rectB)
+end
+
+# The following funtions apply to all creatures.
+position(c::Creature) = c.position
+radius(c::Creature) = c.infection_radius
+name(c::Creature) = c.name
+status(c::Creature) = c.status
+
+# A Person is a subclass of a Creature.
+struct Person<:Creature
+    name::String
+    position::Position
+    radius::Infection_Radius
+    status::Status
+end
+
+
+
+end # corona module
--- main.jl
+++ main.jl
@@ -1,1 +1,20 @@
 include("corona.jl")
+
+# A place to store people.
+people = Array{Person, 1}
+
+for i in 1:10
+
+    pos = corona.Position(rand(1:1000), rand(1:1000))
+    radius = corona.Infection_Radius(rand(5:20), rand(5:20))
+    p1 = corona.Person("Person $i",
+                       pos,
+                       radius,
+                       corona.Healthy)
+
+    println("Name $(p1.name) at ($(p1.position.x),$(p1.position.y))
+        with radius ($(p1.radius.width),$(p1.radius.height))
+        is $(p1.status)")
+
+
+end
