05009cbc59713673c718dee1316dceaaa727f99c
Matthew Weaver <31797463+Wibbo@users.noreply.github.com>
2020-08-07 15:10:06 +0100

    Added compass movement and refactured the main routine.

--- corona.jl
+++ corona.jl
@@ -1,17 +1,26 @@
 # Create a module to avoid namespace collisions.
 module corona
 export Status
+export Direction
 
 # All living entities are a Creature.
 abstract type Creature end
 
 # Define the possible statuses for a person.
 @enum Status Healthy Infected Immune
+@enum Direction North East South West
 
 # Create a struct to hold a person's position.
 mutable struct Position
     x::Int
     y::Int
+end
+
+struct Grid_Area
+    x1::Int64
+    y1::Int64
+    x2::Int64
+    y2::Int64
 end
 
 # Define the physical bounds of each person.
@@ -45,6 +54,7 @@ position(c::Creature) = c.position
 radius(c::Creature) = c.radius
 status(c::Creature) = c.status
 probability(c::Creature) = c.probability
+direction(c::Creature) = c.direction
 
 # A Person is a subclass of a Creature.
 mutable struct Person<:Creature
@@ -53,11 +63,68 @@ mutable struct Person<:Creature
     radius::Int
     status::Status
     probability:: Int
+    direction:: Direction
 end
 
 move_left!(c::Creature, amount::Int)  = c.position.x -= amount
 move_right!(c::Creature, amount::Int) = c.position.x += amount
-move_up!(c::Creature, amount::Int)    = c.position.y -= amount
+move_up!(c::Creature, amount::Int)    = c.position.y += amount
 move_down!(c::Creature, amount::Int)  = c.position.y += amount
+
+function move_north!(c::Creature, min::Int64, max::Int64, g::Grid_Area)
+    dir = Bool(rand(0:1))
+    min1::Int64 = div(min, 2)
+    max1::Int64 = div(max, 2)
+    steps = rand(min:max)
+    steps1 = rand(min1:max1)
+    move_up!(c, steps)
+    dir ? move_left!(c, steps1) : move_right!(c, steps1)
+end
+
+function move_south!(c::Creature, min::Int64, max::Int64, g::Grid_Area)
+    dir = Bool(rand(0:1))
+    min1::Int64 = div(min, 2)
+    max1::Int64 = div(max, 2)
+    steps = rand(min:max)
+    steps1 = rand(min1:max1)
+    move_down!(c, steps)
+    dir ? move_left!(c, steps1) : move_right!(c, steps1)
+end
+
+function move_east!(c::Creature, min::Int64, max::Int64, g::Grid_Area)
+    dir = Bool(rand(0:1))
+    min1::Int64 = div(min, 2)
+    max1::Int64 = div(max, 2)
+    steps = rand(min:max)
+    steps1 = rand(min1:max1)
+    move_right!(c, steps)
+    dir ? move_up!(c, steps1) : move_down!(c, steps1)
+end
+
+function move_west!(c::Creature, min::Int64, max::Int64, g::Grid_Area)
+    dir = Bool(rand(0:1))
+    min1::Int64 = div(min, 2)
+    max1::Int64 = div(max, 2)
+    steps = rand(min:max)
+    steps1 = rand(min1:max1)
+    move_left!(c, steps)
+    dir ? move_up!(c, steps1) : move_down!(c, steps1)
+end
+
+function move_person(c::Creature, min::Int, max::Int, g::Grid_Area)
+    if c.direction == North
+        move_north!(c::Creature, min::Int, max::Int, g::Grid_Area)
+    elseif c.direction == East
+        move_east!(c::Creature, min::Int, max::Int, g::Grid_Area)
+    elseif c.direction == South
+        move_south!(c::Creature, min::Int, max::Int, g::Grid_Area)
+    elseif c.direction == West
+        move_west!(c::Creature, min::Int, max::Int, g::Grid_Area)
+    else
+        throw(InvalidStateException("Invalid direction for movement."))
+    end
+
+end
+
 
 end # corona module
--- main.jl
+++ main.jl
@@ -14,9 +14,30 @@ infected_radius = parse(Int, retrieve(conf, "PEOPLE", "infected_radius"))
 world_width = parse(Int, retrieve(conf, "WORLD", "width"))
 world_height = parse(Int, retrieve(conf, "WORLD", "height"))
 probability = parse(Int, retrieve(conf, "PEOPLE", "probability"))
+min_step = parse(Int, retrieve(conf, "PEOPLE", "min_step"))
+max_step = parse(Int, retrieve(conf, "PEOPLE", "max_step"))
+iterations = parse(Int, retrieve(conf, "WORLD", "iterations"))
+
+grid = corona.Grid_Area(0, 0, world_width, world_height)
 
 #Create a people array.
 people = Array{corona.Person}(undef, total_people)
+
+function get_direction(d::corona.Direction)
+    if d == corona.North
+        "North"
+    elseif d == corona.East
+        "East"
+    elseif d == corona.South
+        "South"
+    elseif d == corona.West
+        "West"
+    else
+        throw(InvalidStateException("Invalid direction for movement."))
+    end
+end
+
+
 
 """
 Creates a person.
@@ -25,8 +46,10 @@ area: A square area around a person that defines when it is possible for them to
 status: The person's status which can be Healthy, Infected or Immune.
 """
 function create_person(name::String; area::Int=10, status::corona.Status=corona.Healthy, prob=20)
-    pos = corona.Position(rand(1:world_width), rand(1:world_height))
-    return corona.Person(name, pos, area, status, prob)
+    pos = corona.Position(rand(0:world_width), rand(0:world_height))
+    dir = rand(0:3)
+    dir2::corona.Direction = corona.Direction(dir)
+    return corona.Person(name, pos, area, status, prob, dir2)
 end
 
 # Create the required number of healthy people.
@@ -41,45 +64,84 @@ for i in healthy_people + 1:infected_people + healthy_people
     people[i] = p
 end
 
-for z in 1:100
+people_counts = [0::Int, 0::Int, 0::Int]
 
-    th = 0
-    ti = 0
-    tm = 0
+"""
+Updates the number of healthy, infected and immune people.
+"""
+function update_people_counts!(c::corona.Creature)
+    if c.status == corona.Healthy
+        people_counts[1] += 1
+    end
 
-    # Analyse the grid to see which healthy people are too close to infected ones.
-    for ip in people
+    if c.status == corona.Infected
+        people_counts[2] += 1
+    end
 
-        if ip.status == corona.Healthy
-            th += 1
-        end
+    if c.status == corona.Immune
+        people_counts[3] += 1
+    end
+end
 
-        if ip.status == corona.Infected
-            ti += 1
-        end
+"""
+Resets the people count vector.
+"""
+function reset_array(a::AbstractVector{Int})
+    a[1] = 0
+    a[2] = 0
+    a[3] = 0
+end
 
-        if ip.status == corona.Immune
-            tm += 1
-        end
+function infect_person!(c::corona.Creature)
+    for hp in people
+        if corona.infection_risk(c, hp)
+            infected = "$(c.name) [$(c.position.x),$(c.position.y)]"
+            healthy = "$(hp.name) [$(hp.position.x),$(hp.position.y)]"
+            println(infected * " danger to " * healthy)
 
-        if ip.status == corona.Infected
-            for hp in people
-                if corona.infection_risk(ip, hp)
-                    infected = "$(ip.name) [$(ip.position.x),$(ip.position.y)]"
-                    healthy = "$(hp.name) [$(hp.position.x),$(hp.position.y)]"
-                    println(infected * " danger to " * healthy)
-
-                    infect = rand(1:100)
-                    if infect < ip.probability
-                        hp.status = corona.Infected
-                    end
-                end
+            infect = rand(1:100)
+            if infect < c.probability
+                hp.status = corona.Infected
             end
         end
     end
 end
 
+"""
+Creates a string containing status information.
+"""
+function report_status!()
+    world_status = "Total: $(total_people) Prob: $(probability) "
+    world_status *= "Radius: $(infected_radius) "
+    world_status *= "World: [$world_width, $world_height]"
 
-println("Total healthy is $(th)")
-println("Total infected is $(ti)")
-println("Total immune is $(tm)")
+    println()
+    println(world_status)
+    println("Total healthy is $(people_counts[1])")
+    println("Total infected is $(people_counts[2])")
+    println("Total immune is $(people_counts[3])")
+end
+
+# This is the main application loop. The outer loop is the number of
+# iterations that are executed. Or, in other words, how many moves
+# each person makes in their digital world.
+for _ in 1:iterations
+    reset_array(people_counts)
+
+    # Analyse the grid to see which healthy people are too close to infected ones.
+    for ip in people
+
+        update_people_counts!(ip)
+
+        if ip.status == corona.Infected
+            infect_person!(ip)
+        end
+
+        corona.move_person(ip, min_step, max_step, grid)
+
+        #corona.move_north!(ip, min_step, max_step, grid)
+        println("$(ip.name) at [$(ip.position.x), $(ip.position.y)] [D:$(ip.direction)]")
+    end
+end
+
+report_status!()
